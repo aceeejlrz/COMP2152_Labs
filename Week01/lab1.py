6dfd9186f943f7cf60c50794c4f93c54e97ca677
aceeejlrz <jzrl.ace.dc@gmail.com>
2026-01-16 14:49:42 -0500

    add lab1.py

---
+++ Week01/lab1.py
@@ -0,0 +1,22 @@
+# sample coding question 01 week 1
+# Jezrel Dela Cruz
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 5: User Input
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 20
+print("Now showing the shop items filtered by age:", userAge)
